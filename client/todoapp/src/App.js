3a1e51b894f75a510b72559b4c7121fef02426fe
Axel Elmarsson <axelelm@kth.se>
2020-03-02 11:51:06 +0100

    Basic registration component

--- client/todoapp/src/App.js
+++ client/todoapp/src/App.js
@@ -1,74 +1,13 @@
 import React from 'react'
 
-const initState = {
-    username: '',
-    password: ''
-}
+import Register from './Register'
 
-class App extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = initState
-
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
-
-    handleChange(e) {
-        this.setState({
-            [e.target.name]:e.target.value
-        })
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-        const {username, password} = this.state
-        
-        const body = JSON.stringify({username, password})
-
-        fetch('http://localhost:4000/register', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body
-        })
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(error => {
-            console.log(error)
-        })
-    }
-
-    render() {
-
-        const {username, password} = this.state;
-
-        return (
-            <div>
-                <form>
-                    <input 
-                        type="text"
-                        name="username"
-                        value={username}
-                        placeholder="Username"
-                        onChange={this.handleChange}
-                    />
-                    <input 
-                        type="password"
-                        name="password"
-                        value={password}
-                        placeholder="Password"
-                        onChange={this.handleChange}
-                    />
-                    <button onClick={this.handleSubmit}>
-                        Sign up
-                    </button>
-                </form>
-            </div>
-        )
-    }
+const App = () => {
+    return (
+        <div>
+            <Register />
+        </div>
+    )
 }
 
 export default App;
---
+++ client/todoapp/src/Register.js
@@ -0,0 +1,87 @@
+import React from 'react'
+
+const initState = {
+    username: '',
+    password: '',
+    password2: '',
+}
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = initState
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const {username, password} = this.state
+        
+        const body = JSON.stringify({username, password})
+
+        fetch('http://localhost:4000/register', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    render() {
+
+        const {username, password, password2} = this.state;
+
+        const isInvalid = username === '' || password === '' || password2 === '' || password !== password2;
+
+        return (
+            <div>
+                <form>
+                    <input 
+                        type="text"
+                        name="username"
+                        value={username}
+                        placeholder="Username"
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type="password"
+                        name="password"
+                        value={password}
+                        placeholder="Password"
+                        onChange={this.handleChange}
+                    />
+                    <input 
+                        type="password"
+                        name="password2"
+                        value={password2}
+                        placeholder="Confirm"
+                        onChange={this.handleChange}
+                    />
+                    <button
+                        onClick={this.handleSubmit}
+                        disabled={isInvalid}
+                    >
+                        Sign up
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Register;
