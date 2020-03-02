907d685831fc75bffd21d10e276d7f75f34b0fc6
Axel Elmarsson <axelelm@kth.se>
2020-03-02 14:00:07 +0100

    Cool navbar

--- client/todoapp/src/App.js
+++ client/todoapp/src/App.js
@@ -3,29 +3,34 @@ import React from 'react'
 import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
 
 import './App.css'
+import './NavBar.css'
 
 import Register from './Register'
 import Login from './Login'
+import NavBar from './NavBar'
 import NotFound from './NotFound'
 
 import * as ROUTES from './routes'
 
 const App = () => {
     return (
-        <div className="App fill-window">
+        <div>
             <BrowserRouter>
-                <Switch>
-                    <Route exact path={ROUTES.HOME}>
-                        <Redirect to={ROUTES.LOGIN}/>
-                    </Route>
-                    <Route exact path={ROUTES.LOGIN}>
-                        <Login />
-                    </Route>
-                    <Route exact path={ROUTES.REGISTER}>
-                        <Register />
-                    </Route>
-                    <Route component={NotFound} />
-                </Switch>
+                <NavBar className="NavBar"/>
+                <div className="App fill-window">
+                    <Switch>
+                        <Route exact path={ROUTES.HOME}>
+                            <Redirect to={ROUTES.LOGIN}/>
+                        </Route>
+                        <Route exact path={ROUTES.LOGIN}>
+                            <Login />
+                        </Route>
+                        <Route exact path={ROUTES.REGISTER}>
+                            <Register />
+                        </Route>
+                        <Route component={NotFound} />
+                    </Switch>
+                </div>
             </BrowserRouter>
         </div>
     )
--- client/todoapp/src/NotFound.js
+++ client/todoapp/src/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import * as ROUTES from './routes'
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
+        <div className="NotFound">
             <h1>404 Not Found</h1>
             The page you are looking for does not exist.
+            <br />
+            <Link to={ROUTES.HOME}>Back to safety</Link>
         </div>
     )
 }
