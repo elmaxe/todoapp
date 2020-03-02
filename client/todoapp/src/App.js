51e64be90989183963abbcbbc25b27c7133e86b6
Axel Elmarsson <axelelm@kth.se>
2020-03-02 12:19:15 +0100

    Client routing

--- client/todoapp/src/App.js
+++ client/todoapp/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
 import Register from './Register'
 import Login from './Login'
+import NotFound from './NotFound'
+
+import * as ROUTES from './routes'
 
 const App = () => {
     return (
         <div>
-            <Register />
-            <Login />
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path={ROUTES.LOGIN}>
+                        <Login />
+                    </Route>
+                    <Route exact path={ROUTES.REGISTER}>
+                        <Register />
+                    </Route>
+                    <Route component={NotFound} />
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
