696a986f9b9563a695b98361a9614297afbba45c
Axel Elmarsson <axelelm@kth.se>
2020-03-02 10:59:50 +0100

    Basic login

--- server/login.js
+++ server/login.js
@@ -4,8 +4,33 @@ var express = require('express')
 var router = express.Router()
 const db = require('./database');
 
-router.get('/', (req, res) => {
-    res.status(404)
+const bcrypt = require('bcrypt')
+
+router.post('/', (req, res) => {
+    const {username, password} = req.body;
+    
+    db.get('SELECT * FROM User WHERE username = ?', [username], (err, row) => {
+        if (err) {
+            res.status(500).json({"error":"Database error"})
+            console.log(err)
+            return
+        }
+
+        if (row === undefined) {
+            res.status(404).json({"error":"Username or password wrong."})
+            return
+        }
+
+        bcrypt.compare(password, row.password, (err, equal) => {
+            if (equal) {
+                res.status(200).json({"status":"Login successful."})
+                return
+            } else {
+                res.status(404).json({"error":"Username or password wrong."})
+                return
+            }
+        })
+    })
 })
 
 module.exports = router
