7ac09028276f7844c3492f0be045d00b944f36bd
Axel Elmarsson <axelelm@kth.se>
2020-03-07 15:34:53 +0100

    Format date and display it in User settings

--- client/todoapp/src/components/Account/Account.js
+++ client/todoapp/src/components/Account/Account.js
@@ -6,11 +6,17 @@ import './Account.css'
 const Account = ({state, actions}) =>  {
     console.log(state)
     console.log(actions)
+    let date = new Date(parseInt(state.auth.user.registrationDate, 10))
+    let year = date.getUTCFullYear()
+    let month = date.getUTCMonth()+1 < 10 ? "0"+(date.getUTCMonth()+1) : date.getUTCMonth()+1
+    let day = date.getUTCDate() < 10 ? "0"+date.getUTCDate() : date.getUTCDate()
+    date = year + "-" + month + "-" + day
+    
     return (
         <div className="Account">
             <div>
                 <h1>{state.auth.user.username}</h1>
-                Member since XXXX
+                Member since {date}
             </div>
             <ChangePassword />
             <RemoveAccount actions={actions} />
