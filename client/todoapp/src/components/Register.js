9199cb7688cbc68db09bd1c33d2a552dc6d1c45d
Axel Elmarsson <axelelm@kth.se>
2020-03-10 16:49:40 +0100

    Protect Register page from logged in users

--- client/todoapp/src/components/Register.js
+++ client/todoapp/src/components/Register.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-import './Login.css'
-import {Link} from 'react-router-dom'
+import {Link, Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
 import * as ROUTES from '../routes'
+import './Login.css'
 import Alert from './Alert'
 
 const initState = {
@@ -68,50 +69,59 @@ class Register extends React.Component {
         const isInvalid = username === '' || password === '' || password2 === '' || password !== password2;
 
         return (
-            <div className="Login">
-                <form className="LoginForm">
-                    <h1>Register</h1>
-                    {error && <Alert type="danger" text={error}/>}
-                    {success && <Alert type="success" text={success + " "} linkText="Login." linkPath={ROUTES.LOGIN} />}
-                    <label>Username</label>
-                    <input
-                        className="inputBox"
-                        type="text"
-                        name="username"
-                        value={username}
-                        placeholder="Username"
-                        onChange={this.handleChange}
-                    />
-                    <label>Password</label>
-                    <input 
-                        className="inputBox"
-                        type="password"
-                        name="password"
-                        value={password}
-                        placeholder="Password"
-                        onChange={this.handleChange}
-                    />
-                    <span><label>Confirm password</label></span>
-                    <input 
-                        className="inputBox"
-                        type="password"
-                        name="password2"
-                        value={password2}
-                        placeholder="Confirm password"
-                        onChange={this.handleChange}
-                    />
-                    <button
-                        className="LoginButton"
-                        onClick={this.handleSubmit}
-                        disabled={isInvalid || fetching}
-                    >
-                        {fetching ? "Registering..." : "Register"}
-                    </button>
-                    <div className="Links">Already have an account? <Link to={ROUTES.LOGIN}>Login.</Link></div>
-                </form>
+            <div>
+                {this.props.state.auth.authenticated ?
+                    // this.props.state.auth.fetching ? null :
+                    <Redirect to={ROUTES.HOME} />
+                : 
+                <div className="Login">
+                    <form className="LoginForm">
+                        <h1>Register</h1>
+                        {error && <Alert type="danger" text={error}/>}
+                        {success && <Alert type="success" text={success + " "} linkText="Login." linkPath={ROUTES.LOGIN} />}
+                        <label>Username</label>
+                        <input
+                            className="inputBox"
+                            type="text"
+                            name="username"
+                            value={username}
+                            placeholder="Username"
+                            onChange={this.handleChange}
+                        />
+                        <label>Password</label>
+                        <input 
+                            className="inputBox"
+                            type="password"
+                            name="password"
+                            value={password}
+                            placeholder="Password"
+                            onChange={this.handleChange}
+                        />
+                        <span><label>Confirm password</label></span>
+                        <input 
+                            className="inputBox"
+                            type="password"
+                            name="password2"
+                            value={password2}
+                            placeholder="Confirm password"
+                            onChange={this.handleChange}
+                        />
+                        <button
+                            className="LoginButton"
+                            onClick={this.handleSubmit}
+                            disabled={isInvalid || fetching}
+                        >
+                            {fetching ? "Registering..." : "Register"}
+                        </button>
+                        <div className="Links">Already have an account? <Link to={ROUTES.LOGIN}>Login.</Link></div>
+                    </form>
+                </div>
+                }
             </div>
         )
     }
 }
 
-export default Register;
+export default connect(
+    state => ({state})
+)(Register);
